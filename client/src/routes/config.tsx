fb078f616a773cbc6ba66062a830040b4bc08b7a
Davit Balayan <davit.balayan@cometanalysis.com>
2025-06-16 19:15:18 +0400

    fixed react router to support base

--- client/src/routes/config.tsx
+++ client/src/routes/config.tsx
@@ -2,14 +2,15 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "src/App";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App />,
-      children: [],
-    },
-    {
-      basename: "/usernet",
-    }
-  ]);
-  
-  export default router
+  {
+    path: "/",
+    element: <App />,
+    children: [],
+  },
+],
+  {
+    basename: "/usernet",
+  }
+);
+
+export default router
